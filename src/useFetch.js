66063eaf3a783c8d3ab7601ed794d3cf17729084
sajani-p <sajipathegama@gmail.com>
2021-07-17 22:40:29 +0530

    add the dojo-blog

---
+++ src/useFetch.js
@@ -0,0 +1,42 @@
+import { useState,useEffect } from 'react';
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const abortCont = new AbortController(); // if we click home and at that moment if we quickly click the new blog it gave a error. to catch that error we use abortController
+
+        setTimeout(() => {
+            fetch(url, { signal: abortCont.signal })
+                .then(res => {
+                    if(!res.ok){  // error coming back from server
+                        throw Error('Could not fetch the data for that resource');
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setIsPending(false);
+                    setData(data);
+                    setError(null);
+                })
+                .catch(err => {
+                    if (err.name === 'AbortError') {
+                        console.log('Fetch aborted');
+                    }
+                    else{
+                        setIsPending(false);
+                        setError(err.message);
+                    }
+                })
+        }, 1000 );
+        
+        // abort the fetch
+        return () => abortCont.abort();
+    }, [url]);
+
+    return { data, isPending, error }
+}
+
+export default useFetch;
